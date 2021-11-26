72d1dcc1404e24d798884070509ad1c648748ea6
martian17 <yakumodanshi@gmail.com>
2021-11-26 11:39:07 +0900

    modified the interface for test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let parse = require("./parser.js");
 
 module.exports = {
     parse:function(str){
-        console.log("parsing your text owo awa");
+        console.log("parsing your text owo awa aha");
         return parse(str,0);
     }
 };
